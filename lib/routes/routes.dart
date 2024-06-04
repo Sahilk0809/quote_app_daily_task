e2552b30a6341426e39ec94d4871e5e38533c014
Sahil <rk4852115@gmail.com>
2024-06-04 13:02:09 +0530

    04/06/2024

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:quote_app_daily_task/dialoge/dialogeBox.dart';
 import 'package:quote_app_daily_task/invoice/invoiceGenerator.dart';
+import 'package:quote_app_daily_task/quotes/quotes.dart';
 import '../invoice/details.dart';
 import '../invoice/pdf.dart';
 
 class MyRoutes{
   static Map<String, Widget Function(BuildContext)> myRoutes = {
     '/dialoge': (context) => const DialogueScreen(),
-    '/': (context) => const InvoiceGenerator(),
+    '/invoice': (context) => const InvoiceGenerator(),
     '/detail': (context) => const DetailScreen(),
     '/pdf': (context) => const PdfScreen(),
+    '/': (context) => const QuotesScreen(),
   };
 }
